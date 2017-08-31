7a21f4b127c105f9d60b4e7dddb81a6dcce427c7
pcruaud <perrine.cruaud@gmail.com>
2017-08-31 12:11:00 -0400

    FewCorrections

--- Summary_RunQuality.sh
+++ Summary_RunQuality.sh
@@ -53,15 +53,15 @@ echo -e "Gene $(($i+1))\n$NomGene\nLongueur fragment $LongueurFragment bp\nAmorc
 echo "Gene $(($i+1))    $NomGene    $LongueurFragment   $AmorceForward  $AmorceReverse  $Recouvrement" >> CaracteristiquesFragmentsSequences.txt
 done
 
-sed -n '/Total Sequences/p' Bilan_global/Global_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
-TotSequencesBrut=$(sed -n '/Total Sequences/p' Bilan_global/Global_fastqc/fastqc_data.txt)
+sed -n '/Total Sequences/p' Bilan_Global/Global_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
+TotSequencesBrut=$(sed -n '/Total Sequences/p' Bilan_Global/Global_fastqc/fastqc_data.txt)
 TotSequences=$(sed s:'Total Sequences':'':g <<< $TotSequencesBrut)
 sed -i -e "s/Total Sequences	/Nombre total de sequences : /g" CaracteristiquesRun.txt
 
-sed -n '/Sequence length	/p' Bilan_global/Global_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
+sed -n '/Sequence length	/p' Bilan_Global/Global_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
 sed -i -e "s/Sequence length	/Longueurs des sequences (bp) : /g" CaracteristiquesRun.txt
 
-sed -n '/>>Per sequence quality scores/{:a;N;/>>END_MODULE/!ba;p;}' Bilan_global/Global_fastqc/fastqc_data.txt > Quality.temp
+sed -n '/>>Per sequence quality scores/{:a;N;/>>END_MODULE/!ba;p;}' Bilan_Global/Global_fastqc/fastqc_data.txt > Quality.temp
 sed -i -e '1d' Quality.temp
 sed -i -e '2d' Quality.temp
 sed -i -e '$d' Quality.temp
@@ -71,15 +71,15 @@ PercentQ30Temp=$(bc <<< "scale=3;$SeqSupQ30 / $TotSequences")
 PercentQ30=$(bc <<< "$PercentQ30Temp * 100")
 echo -e "Pourcentage de sequences dont la qualite est superieure a Q30 moyen (%): $PercentQ30" >> CaracteristiquesRun.txt
 
-sed -n '/Total Sequences/p' Bilan_global/Global_Reads1_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
-TotSequencesBrutR1=$(sed -n '/Total Sequences/p' Bilan_global/Global_Reads1_fastqc/fastqc_data.txt)
+sed -n '/Total Sequences/p' Bilan_Global/Global_Reads1_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
+TotSequencesBrutR1=$(sed -n '/Total Sequences/p' Bilan_Global/Global_Reads1_fastqc/fastqc_data.txt)
 TotSequencesR1=$(sed s:'Total Sequences':'':g <<< $TotSequencesBrutR1)
 sed -i -e "s/Total Sequences	/Nombre total de reads 1 : /g" CaracteristiquesRun.txt
 
-sed -n '/Sequence length	/p' Bilan_global/Global_Reads1_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
+sed -n '/Sequence length	/p' Bilan_Global/Global_Reads1_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
 sed -i -e "s/Sequence length	/Longueurs des sequences (bp) : /g" CaracteristiquesRun.txt
 
-sed -n '/>>Per sequence quality scores/{:a;N;/>>END_MODULE/!ba;p;}' Bilan_global/Global_Reads1_fastqc/fastqc_data.txt > QualityR1.temp
+sed -n '/>>Per sequence quality scores/{:a;N;/>>END_MODULE/!ba;p;}' Bilan_Global/Global_Reads1_fastqc/fastqc_data.txt > QualityR1.temp
 sed -i -e '1d' QualityR1.temp
 sed -i -e '2d' QualityR1.temp
 sed -i -e '$d' QualityR1.temp
@@ -89,15 +89,15 @@ PercentQ30TempR1=$(bc <<< "scale=3;$SeqSupQ30R1 / $TotSequencesR1")
 PercentQ30R1=$(bc <<< "$PercentQ30TempR1 * 100")
 echo -e "Pourcentage de reads 1 dont la qualite est superieure a Q30 moyen (%): $PercentQ30R1" >> CaracteristiquesRun.txt
 
-sed -n '/Total Sequences/p' Bilan_global/Global_Reads2_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
-TotSequencesBrutR2=$(sed -n '/Total Sequences/p' Bilan_global/Global_Reads2_fastqc/fastqc_data.txt)
+sed -n '/Total Sequences/p' Bilan_Global/Global_Reads2_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
+TotSequencesBrutR2=$(sed -n '/Total Sequences/p' Bilan_Global/Global_Reads2_fastqc/fastqc_data.txt)
 TotSequencesR2=$(sed s:'Total Sequences':'':g <<< $TotSequencesBrutR2)
 sed -i -e "s/Total Sequences	/Nombre total de reads 2 : /g" CaracteristiquesRun.txt
 
-sed -n '/Sequence length	/p' Bilan_global/Global_Reads2_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
+sed -n '/Sequence length	/p' Bilan_Global/Global_Reads2_fastqc/fastqc_data.txt >> CaracteristiquesRun.txt
 sed -i -e "s/Sequence length	/Longueurs des sequences (bp) : /g" CaracteristiquesRun.txt
 
-sed -n '/>>Per sequence quality scores/{:a;N;/>>END_MODULE/!ba;p;}' Bilan_global/Global_Reads2_fastqc/fastqc_data.txt > QualityR2.temp
+sed -n '/>>Per sequence quality scores/{:a;N;/>>END_MODULE/!ba;p;}' Bilan_Global/Global_Reads2_fastqc/fastqc_data.txt > QualityR2.temp
 sed -i -e '1d' QualityR2.temp
 sed -i -e '2d' QualityR2.temp
 sed -i -e '$d' QualityR2.temp
